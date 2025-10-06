65397c0be95317a801da1a62965f03655c601faa
BX37 <123464815+BX37@users.noreply.github.com>
2025-10-06 19:18:00 +0200

    Modif FrontEnd to FrontEndReact

--- MyContacts/frontendreact/src/index.js
+++ MyContacts/frontendreact/src/index.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // si tu as du CSS global
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
